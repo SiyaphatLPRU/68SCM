941705cf9efab0c5f3c743c857e4ed9d8c3a8d0c
siyaphat <siyaphat@g.lpru.ac.th>
2025-11-24 11:12:18 +0700

    commit main_login.php

---
+++ main_login.php
@@ -0,0 +1,14 @@
+<?php
+// /C:/xampp/htdocs/SCM/index.php
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SCM</title>
+</head>
+<body>
+    <?php echo 'page login for customer' ?>
+    
+</body>
+</html>
