6da3a6fc31489e067e2022df559e1b0682d1a6fb
siyaphat <siyaphat@g.lpru.ac.th>
2025-11-24 10:31:04 +0700

    first file name = index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// /C:/xampp/htdocs/SCM/index.php
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SCM</title>
+</head>
+<body>
+    <?php echo 'hello'; ?>
+</body>
+</html>
